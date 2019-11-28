9c91c4b4152741772c82767580b7f04372f6df8d
DomenicW <ms.kreis8@t-online.de>
2019-11-28 15:03:08 +0100

    example part 2

--- example/bitly/index.js
+++ example/bitly/index.js
@@ -1,9 +1,8 @@
-// EXAMPLE CODE
 var bitfire = require("bitfire");
 var readline = require('readline');
 var fs = require('fs');
 
-let possibleChars = "abcdefghijklmnopqrstuvwxyz";
+// Read proxy.txt
 var filename = "proxy.txt";
 var proxys = [];
 readline.createInterface({
@@ -12,25 +11,9 @@ readline.createInterface({
 }).on('line', function(line) {
     proxys.push(line)
 });
-function genrnd(n) {
-    var result = '';
-    for (var i = 0; i < n; i++) {
-        result += possibleChars.charAt(Math.floor(Math.random() * 26));
-    }
-	if(result != ""){
-    return result;
-	}else{
-		return generateCode();
-	}
-}
-function generateCode(){
-	nf = "https://discordapp.com/api/v6/invites/" + genrnd(3) +"?with_counts=true";
-	return nf;
-}
 
-// BEGIN BRUTE
+// Do the actual Bruteforcing
 bitfire.proxyBrute(function(){return "http://bit.ly/" + bitfire.rndStr(bitfire.lowerCaseChars,6)},proxys,30,function(error,response,body,code){
- // console.log(error,response,body,code)
  try{
  if(response.statusCode == 200){
 	 console.log("Found Invite: " + code)
