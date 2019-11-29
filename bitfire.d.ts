c0ea208f76b9b9c32d4c1f305e19c1033f29f45c
Gecko <55274516+iamgecko@users.noreply.github.com>
2019-11-29 11:38:23 +0100

    bro typescript

---
+++ bitfire.d.ts
@@ -0,0 +1,16 @@
+export const charsNum: string;
+
+export const lowCharsNum: string;
+
+export const lowerCaseChars: string;
+
+export const mixedCaseChars: string;
+
+export const upCharsNum: string;
+
+export const upperCaseChars: string;
+
+export function proxyBrute(generateCode: any, proxyArray: any, interval: any, callback: any): void;
+
+export function rndStr(e: any, n: any): any;
+
